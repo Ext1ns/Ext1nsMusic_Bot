ff030538c4a720e3e5bad08567001f473bf59bb1
Ext1ns <salamovgeorgi0@gmail.com>
2024-05-19 16:28:48 +0300

    Обновлён основной файл main

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from config import TOKEN
 from aiogram import Bot, Dispatcher
 from handlers.private import private_router
 from aiogram.enums import ParseMode
-
+from download_music.music import music_router
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
 dp = Dispatcher()
-dp.include_routers(private_router)
+dp.include_routers(private_router, music_router)
 
 
 async def main():
